4781e7526971a97eea08ad5a7535bf2804673641
AntonDershen <dershen95@gmail.com>
2015-12-03 13:37:48 +0300

    add database

---
+++ FitnessClub/FitnessClub/FitnessClub/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessClub.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
